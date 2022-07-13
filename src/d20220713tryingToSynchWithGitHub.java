a0f5910acd00fa9e389f2bb15cbc357a66b33fe0
AntonVotinov <votinovanton@gmail.com>
2022-07-13 21:02:20 +0300

    commit from Dell

--- src/d20220713tryingToSynchWithGitHub.java
+++ src/d20220713tryingToSynchWithGitHub.java
@@ -3,5 +3,6 @@ public class d20220713tryingToSynchWithGitHub {
         System.out.println("What's up guys?");
         System.out.println("Trying to create project on MSI laptop, push to github and open on DELL laptop");
         System.out.println("MSI - ok, and it pushed successfully to GitHub (checked) twice");
+        System.out.println("Ok, success, now i opened it on DELL via main enter menu of IDE, closed project before that");
     }
 }
