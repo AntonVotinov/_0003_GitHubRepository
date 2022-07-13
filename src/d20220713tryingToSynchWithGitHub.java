e1412f12ac5555b5c16f3dab2e6e274760edd787
AntonVotinov <votinovanton@gmail.com>
2022-07-13 20:27:55 +0300

    Initial commit

---
+++ src/d20220713tryingToSynchWithGitHub.java
@@ -0,0 +1,6 @@
+public class d20220713tryingToSynchWithGitHub {
+    public static void main(String[] args) {
+        System.out.println("What's up guys?");
+        System.out.println("Trying to create project on MSI laptop, push to github and open on DELL laptop");
+    }
+}
