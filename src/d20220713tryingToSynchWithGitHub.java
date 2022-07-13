1dcd756aa37fdcd1b50b1f44e554e190971f90a7
AntonVotinov <votinovanton@gmail.com>
2022-07-13 21:05:14 +0300

    commit from MSI

--- src/d20220713tryingToSynchWithGitHub.java
+++ src/d20220713tryingToSynchWithGitHub.java
@@ -4,5 +4,6 @@ public class d20220713tryingToSynchWithGitHub {
         System.out.println("Trying to create project on MSI laptop, push to github and open on DELL laptop");
         System.out.println("MSI - ok, and it pushed successfully to GitHub (checked) twice");
         System.out.println("Ok, success, now i opened it on DELL via main enter menu of IDE, closed project before that");
+        System.out.println("This is the final testing. I opened it on MSI (successfully) and now amending one more time to check, huyha!");
     }
 }
